f6ee6ceb781339a81dca2343c22d803a7ce57b7b
TakuroGunji <13ec037@ms.dendai.ac.jp>
2015-10-13 20:38:48 +0900

    test

--- serch.java
+++ serch.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
-class BinSearch {
-	static int binSearch (int[] a, int n ,int key)
+class SearchIdx {
+	static int serchIdx (int[] a, int n ,int key,int[] idxx)
 	{
 		int pl = 0;
 		int pr = n-1;
@@ -19,6 +19,23 @@ class BinSearch {
 		return -1; //探索失敗
 	}
 	
+	static int searchIdx(int []a,int n, int key ,int [] idx){
+		int id=0;
+		
+		for(int i=0; i <= n; i++){
+			if(i == n){
+				return id;
+			}
+			
+			else if(a [i] == key){
+				idx[id] = a[i];
+								id++;
+			}
+		}
+		return -1;
+		
+	}
+	
 	public static void main(String[] args){
 		Scanner stdIn = new Scanner(System.in);
 		
@@ -26,28 +43,31 @@ class BinSearch {
 		int num = stdIn.nextInt();
 		
 		int [] x = new int[num];
+		int [] d = new int[num];  //一致するインデックスを先頭fから格納
 		
-		System.out.println("昇順に入力してください");
+		System.out.println("入力してください");
 		
 		System.out.print("x[0]:");
 		x[0] = stdIn.nextInt();
 		
 		for(int i = 1; i < num; i++){
-			do{
+		
 				System.out.println("x["+i+"]:");
 				x[i] = stdIn.nextInt();
-			}while(x[i] < x[i -1]);
-			
 		}
+			
 		
 	System.out.print("探す値:");
 	int ky = stdIn.nextInt();
 	
-	int idx = binSearch(x, num,ky);
+	
+	
+	int idx = searchIdx(x, num,ky,d);
 	
 	if(idx == -1)
 		System.out.println("その値の要素は存在しません。");
 	else
-		System.out.println("その値はx[["+ idx +"]にあります");
-	}
+		System.out.println("その値は"+ idx +"個存在します。");
 }
+		}
+
